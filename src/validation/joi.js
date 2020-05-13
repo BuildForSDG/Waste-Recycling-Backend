6f1f7327fb51dc090b760475fe5cc2d6144fb41c
lamido <kinglamido@gmail.com>
2020-05-13 04:07:00 +0100

    feat(user-login):add login route, validation of user model, model and user route test [finishes #3]

--- src/validation/joi.js
+++ src/validation/joi.js
@@ -2,10 +2,6 @@
 import { BadRequest } from '../errors';
 
 export const validate = async (schema, payload) => {
-  try {
-    const validateAsync = await schema.validateAsync(payload, { abortEarly: false });
-    return validateAsync;
-  } catch (error) {
-    throw new BadRequest(error);
-  }
+  await schema.validateAsync(payload, { abortEarly: false })
+    .catch((error) => { throw new BadRequest(error); });
 };

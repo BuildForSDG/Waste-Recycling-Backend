aa2d2389a22338d43a29782122f52c94aabc6017
Ayobami <adelekeayobami13@gmail.com>
2020-05-30 11:18:23 +0100

    delete and view org

--- src/routes/org.js
+++ src/routes/org.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
-import { createOrg, orglogIn, orgProfileUpdate } from '../controllers';
+import {
+  createOrg, orglogIn, orgProfileUpdate, viewOrgProfile, deleteOrg
+} from '../controllers';
 import {
   catchAsync, guest, authAccount, cloudinary, multerUploadSingle
 } from '../middleware';
@@ -10,5 +12,9 @@ router.post('/create-org', guest, catchAsync(createOrg));
 router.post('/login-org', guest, catchAsync(orglogIn));
 
 router.patch('/org/:id', authAccount, cloudinary, multerUploadSingle, catchAsync(orgProfileUpdate));
+
+router.delete('/org/:id', authAccount, catchAsync(deleteOrg));
+
+router.get('/org/:id', catchAsync(viewOrgProfile));
 
 export default router;
